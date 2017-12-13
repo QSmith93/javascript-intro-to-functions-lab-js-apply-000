a7eafdec8e6efda1b03419922eb0bdc0b36f0637
Quentin <q.smith1@yahoo.com>
2017-12-13 04:35:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function shout(string) {
-  returns string.toUpperCase()
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return ("I can't hear you!")
+  } else if (string === string.toUpperCase()) {
+    return ("YES INDEED!")
+  } else {
+    return ("I love you, too.")
+  }
 }
